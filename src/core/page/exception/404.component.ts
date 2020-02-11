6f8d23112a4505e080b5de31f27c5efa842993c4
rongguohao <843468011@qq.com>
2020-02-11 22:25:40 +0800

    1

---
+++ src/core/page/exception/404.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+    selector: 'exception-404',
+    templateUrl: './404.component.html'
+})
+export class Exception404Component {
+    
+    constructor() {
+
+    }
+}
