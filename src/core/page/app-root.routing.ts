3447e5185a8085f4d9ffc8bf44a63939e199853e
rongguohao <843468011@qq.com>
2020-02-12 18:13:39 +0800

    1

--- src/core/page/app-root.routing.ts
+++ src/core/page/app-root.routing.ts
@@ -19,9 +19,16 @@ const routes: Routes = [
     component: MainComponent,
     children: [
       { path: 'dashboard', loadChildren: '../../app/dashboard/index#DashboardModule', data: { breadcrumb: '数据看板' } },
-      { path: 'user', loadChildren: '../../app/setting-manage/user/index#UserModule', data: { breadcrumb: '用户管理' } },
-      { path: 'app', loadChildren: '../../app/setting-manage/application/index#ApplicationModule', data: { breadcrumb: '应用管理' } },
-      { path: 'auth', loadChildren: '../../app/setting-manage/auth/index#AuthModule', data: { breadcrumb: '权限管理' } }
+      {
+        path: 'setting',
+        data: { breadcrumb: '设置' },
+        children: [
+          { path: 'user', loadChildren: '../../app/setting-manage/user/index#UserModule', data: { breadcrumb: '用户管理' } },
+          { path: 'app', loadChildren: '../../app/setting-manage/application/index#ApplicationModule', data: { breadcrumb: '应用管理' } },
+          { path: 'auth', loadChildren: '../../app/setting-manage/auth/index#AuthModule', data: { breadcrumb: '权限管理' } }
+        ]
+      }
+
     ]
   }
 ];
