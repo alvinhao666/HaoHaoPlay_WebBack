e604c89f95b105baadba8666a446f54cd12dbaac
rongguohao <843468011@qq.com>
2020-02-13 20:29:15 +0800

    1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit, Type } from '@angular/core';
 
-import { Title } from '@angular/platform-browser';
 import { Router, NavigationError, NavigationCancel, NavigationStart } from '@angular/router';
 import { NzMessageService } from 'ng-zorro-antd';
 
@@ -14,7 +13,6 @@ export class AppComponent implements OnInit {
     componentConent: Type<any>;
     constructor(
         private router: Router,
-        private titleService: Title,
         public msg: NzMessageService,
     ) {
         this.routerHandle();
@@ -22,7 +20,7 @@ export class AppComponent implements OnInit {
 
 
     ngOnInit() {
-        this.titleService.setTitle('好好玩后台系管理统');
+        // this.titleService.setTitle('好好玩后台系管理统');
         // this.viewSwitching();
     }
 
