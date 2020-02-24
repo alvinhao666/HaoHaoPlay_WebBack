a4a212aae87c39fc8eb35feb18a29f74be2d43bb
rongguohao <rongguohao@vip56.cn>
2020-02-24 14:35:18 +0800

    1

---
+++ src/app/modules/personal/personal.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {PersonalComponent} from './personal.component';
+
+
+@NgModule({
+  declarations: [PersonalComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class PersonalModule { }
