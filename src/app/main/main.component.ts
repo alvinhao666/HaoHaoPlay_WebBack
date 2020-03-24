bab7fc9e1e0bd73e67ea3da8b07eb3ba72e4cb5e
rongguohao <rongguohao@vip56.cn>
2020-03-24 17:09:15 +0800

    1

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -35,8 +35,7 @@ export class MainComponent extends CoreContainer implements OnInit {
       this.name = d.Name;
       this.firstName = this.name.substring(0, 1);
       this.firstNameBgColor = getColorByFirstName(d.FirstNameSpell);
-      const time = new Date().getTime();
-      this.headImgUrl = environment.api_url + `AvatarFile/${d.HeadImgUrl}?time=${time}`;
+      this.headImgUrl = environment.api_url + `AvatarFile/${d.HeadImgUrl}`;
     });
   }
 
