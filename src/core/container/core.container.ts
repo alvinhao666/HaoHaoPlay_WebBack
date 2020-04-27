576020ede8331f154847c778652b1643ef505d4e
rongguohao <rongguohao@vip56.cn>
2020-04-27 16:20:02 +0800

    1

--- src/core/container/core.container.ts
+++ src/core/container/core.container.ts
@@ -9,7 +9,11 @@ export class CoreContainer extends Core {
     // 检查权限
     CheckAuth(authCode: string): boolean {
         const auths = authCode.split('_');
-        const index = parseInt(auths[0], 10) - 1;
+        if (auths.length < 2) return false;
+        const layer = parseInt(auths[0], 10);
+        if (isNaN(layer)) return false;
+        const index = layer - 1;
+        if (index < 0) return false;
         const num = Number(auths[1]);
         const authNum = Number(Core.authNums[index]);
         return (num & authNum) === num;
