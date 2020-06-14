b6ed73986ba977113988baff8d9e0dabd65b410e
rongguohao <843468011@qq.com>
2020-06-14 21:47:03 +0800

    1

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
-  api_url: 'http://localhost:8000/',
+  //api_url: 'http://localhost:8000/', docker
+  api_url: './Api/',
   token_key: 'HaoToken',
   user_key: 'HaoUser',
   production: true
