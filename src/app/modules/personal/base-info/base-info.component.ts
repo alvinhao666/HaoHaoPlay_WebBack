91342bd96e2febf29b10c9141d5c14603cc41bed
rongguohao <843468011@qq.com>
2020-12-07 22:00:00 +0800

    1

--- src/app/modules/personal/base-info/base-info.component.ts
+++ src/app/modules/personal/base-info/base-info.component.ts
@@ -27,25 +27,27 @@ export class BaseInfoComponent extends CoreEdit implements OnInit {
 
   firstNameBgColor = '';
 
-  formatterAge = value => value && `${value}`;
-  parserAge = value => value && value.replace('.', '');
+  // formatterAge = value => value && `${value}`;
+  // parserAge = value => value && value.replace('.', '');
 
-
-  get fAge() {
-    return this.form.controls.fAge;
-  }
-
-  get fGender() {
-    return this.form.controls.fGender;
-  }
 
   get fName() {
     return this.form.controls.fName;
   }
 
-  get fNickName() {
-    return this.form.controls.fNickName;
+
+  get fGender() {
+    return this.form.controls.fGender;
   }
+
+
+  get fPhone() {
+    return this.form.controls.fPhone;
+  }
+  get fWeChat() {
+    return this.form.controls.fWeChat;
+  }
+
   get fProfile() {
     return this.form.controls.fProfile;
   }
@@ -62,10 +64,12 @@ export class BaseInfoComponent extends CoreEdit implements OnInit {
     private userInfoSubject: UserInfoSubject) {
     super();
     this.form = this.fb.group({
-      fName: [null, Validators.required],
-      fNickName: [null],
+      fName: [{ value: null, disabled: true }, Validators.required],
+
       fGender: [null],
-      fAge: [null, Validators.required],
+      fPhone: [null, [Validators.required, Validators.pattern(/^(13[0-9]|14[579]|15[0-3,5-9]|16[6]|17[0135678]|18[0-9]|19[89])\d{8}$/)]],
+      fWeChat: [null],
+      // fAge: [null, Validators.required],
       fProfile: [null],
       fHomeAddress: [null]
     });
@@ -85,9 +89,11 @@ export class BaseInfoComponent extends CoreEdit implements OnInit {
     // this.setFirstName(user.Name.substring(0, 1));
     // this.setFirstNameBgColor(getColorByFirstName(user.FirstNameSpell));
     this.form.get('fName').setValue(user.Name);
-    this.form.get('fNickName').setValue(user.NickName);
+
     this.form.get('fGender').setValue(user.Gender.toString());
-    this.form.get('fAge').setValue(user.Age);
+    this.form.get('fPhone').setValue(user.Phone);
+    this.form.get('fWeChat').setValue(user.WeChat);
+    // this.form.get('fAge').setValue(user.Age);
     this.form.get('fProfile').setValue(user.Profile);
     this.form.get('fHomeAddress').setValue(user.HomeAddress);
 
@@ -97,10 +103,11 @@ export class BaseInfoComponent extends CoreEdit implements OnInit {
     if (!this.checkForm(this.form)) return;
     this.loading = true;
     this.http.put(`CurrentUser/UpdateBaseInfo`, {
-      Name: this.fName.value,
-      NickName: this.fNickName.value,
-      Gender: parseInt(this.fGender.value, 10),
-      Age: this.fAge.value,
+      // Name: this.fName.value,
+      Phone: this.fPhone.value,
+      // Gender: parseInt(this.fGender.value, 10),
+      // Age: this.fAge.value,
+      WeChat: this.fWeChat.value,
       Profile: this.fProfile.value,
       HomeAddress: this.fHomeAddress.value
     }).subscribe(d => {
