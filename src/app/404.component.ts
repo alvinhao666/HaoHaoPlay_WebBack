f2f54f96f03b4940d76b0a1a2a037a14f40bbb73
rongguohao <843468011@qq.com>
2020-12-21 20:00:42 +0800

    1

--- src/app/404.component.ts
+++ src/app/404.component.ts
@@ -3,17 +3,17 @@ import { Location } from '@angular/common';
 
 @Component({
     selector: 'app-page-404',
-    template: `<div class="exception404">
-                <div class="exception-img-block">
-                    <div class="exception__img" style="background-image: url('../../assets/img/404.svg');background-color : #f0f2f5;"></div>
+    template: `
+                <div class="exception404">
+                    <div class="exception-img-block">
+                        <div class="exception-img" style="background-image: url('../../assets/img/404.svg');background-color : #f0f2f5;"></div>
+                    </div>
+                    <div class="exception-cont">
+                        <h1 class="exception-cont-title">404</h1>
+                        <div class="exception-cont-desc">抱歉，你访问的页面不存在</div>
+                        <button class="ant-btn ant-btn-primary ng-star-inserted" (click)="location.back()"><span class="ng-star-inserted">返回</span></button>
+                    </div>
                 </div>
-                <div class="exception__cont">
-                    <h1 class="exception__cont-title">404</h1>
-                    <div class="exception__cont-desc">抱歉，你访问的页面不存在</div>
-                    <button nz-button="" class="ant-btn ant-btn-primary ng-star-inserted" (click)="location.back()"><span class="ng-star-inserted">返回</span></button>
-                </div>
-              </div>
-   
               `,
     styles: [`
         .exception404{
@@ -29,7 +29,7 @@ import { Location } from '@angular/common';
             padding-right: 152px;
             zoom: 1;
         }
-        .exception__img {
+        .exception-img {
             float: right;
             width: 100%;
             max-width: 430px;
@@ -38,14 +38,14 @@ import { Location } from '@angular/common';
             background-position: 50% 50%;
             background-size: 100% 100%;
         }
-        .exception__cont-title {
+        .exception-cont-title {
             margin-bottom: 24px;
             color: #434e59;
             font-weight: 600;
             font-size: 72px;
             line-height: 72px;
         }
-        .exception__cont-desc {
+        .exception-cont-desc {
             margin-bottom: 16px;
             color: rgba(0,0,0,.45);
             font-size: 20px;
